af70564fd21552e4fd96c81fd64f60731bbc1b43
Tummas Jóhan Sigvardsen <tsigva16@student.aau.dk>
2020-05-17 20:15:20 +0200

    Lecture 2 complete
    [v] 1.Select a bar (find name, logo, image...)
    [v] 2.Design your app (first screen) on paper
    [v] 3.Create Your Bar project (remember an empty activity)
      • Change the app title 
      • Change the app icon
    [v ]4.Modify the first Activity to be an intro page
      • Include a welcome message and image (externalised resources)
    [v] 5.Implement the 6 lifecycle callback methods: onCreate(),onStart(),onResume(),onPause(),onStop(), and onDestroy() 
    •Write a Log message in each of these methods and look up in the logcat to see when they a called..

--- app/src/main/java/com/example/alaborg/MainActivity.java
+++ app/src/main/java/com/example/alaborg/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.alaborg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("Information", "onCreate.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i("Information", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        Log.i("Information", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop(){
+        Log.i("Information", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i("Information", "onDestory");
+        super.onDestroy();
     }
 }
